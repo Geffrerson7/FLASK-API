863935b99cbfa8df079812e216fa08ab226eb9b1
Gefferson Casasola <gcasasolah@uni.pe>
2023-05-19 01:28:14 -0500

    Add API documentation

--- app/__init__.py
+++ app/__init__.py
@@ -4,15 +4,26 @@ from .routes.index import projects_router
 from app.db import db
 from app.serializer import ma
 from flask_cors import CORS
+from flask_smorest import Api
 
+# def create_app():
+    
+#     app = Flask(__name__)
+#     app.config.from_object(Config)
+#     CORS(app, origins='*')
+#     app.register_blueprint(projects_router)
+#     db.init_app(app)
+#     ma.init_app(app)
+        
+#     return app
 
 def create_app():
-    
     app = Flask(__name__)
     app.config.from_object(Config)
     CORS(app, origins='*')
-    app.register_blueprint(projects_router)
+    api = Api(app)
+    api.register_blueprint(projects_router)
     db.init_app(app)
     ma.init_app(app)
-        
+
     return app
